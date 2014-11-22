be283cd2b8764a647b730bbf2103ab8f72e45e8a
TREVOR WILLIAMS <trevorcwilliams@gmail.com>
2014-11-21 19:32:14 -0800

    first

---
+++ TruePhoto/TruePhoto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TruePhoto.Startup))]
+namespace TruePhoto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
